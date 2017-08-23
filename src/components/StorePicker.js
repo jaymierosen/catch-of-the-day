c4fcd985eb43e1174bd568aa46d60b0a7d9ce62b
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-08-23 19:42:31 -0400

    added routing to visit store button

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,16 +3,40 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	// constructor() {
+		// super();
+		//{/* this is equal to the StorePicker component */}
+		// this.goToStore = this.goToStore.bind(this);
+	// }
+	goToStore(e) {
+		// e.preventDefault();
+		//first, grab the text from the box
+		//second, going to transition from / to /store/:storeId
+		// const storeId = this.storeInput.value;
+		// this.context.router.transitionTo(`/store${storeId}`);
+
+		event.preventDefault();
+		console.log('You Changed the URL');
+		// first grab the text from the box
+		const storeId = this.storeInput.value;
+		// second we're going to transition from / to /store/:storeId
+		this.context.router.transitionTo(`/store/${storeId}`);
+	}
 	render() {
-		// Any where else
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
+				{/* $r.storeInput is the input dom element */}
 				<button type="submit">Visit Store →</button>
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	//tell react the storepicker component expects a router
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
